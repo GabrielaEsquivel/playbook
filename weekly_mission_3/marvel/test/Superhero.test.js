a6b84bfc56eabf92f46cdaebe5f8ce4e1d20742b
Andrea <andrea.rueda-ext@dominion-global.com>
2022-04-18 21:35:31 -0500

    create test with no class

---
+++ weekly_mission_3/marvel/test/Superhero.test.js
@@ -0,0 +1,12 @@
+
+describe("Unit Case For Superhero Class", ()=>{
+    test('Case test: Create a superhero', () => {
+        const captainAmerica = new Superhero("Captain America");
+        expect(captainAmerica.nameHero).toBe("Captain America");
+        expect(captainAmerica.civilName).toBe("Steeve Roggers");
+        expect(captainAmerica.actor).toBe("Chris Evans")
+
+
+        expect().toBe()
+    })
+})
